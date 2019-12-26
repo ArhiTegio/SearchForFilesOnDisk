e3e0ce5630d55e1c599d7e0a5de4797bdeb2ca06
Arhio <Arhio@yandex.ru>
2019-12-26 23:48:11 +0300

    ГК АРМО. Задание для собиседования. Описание половины логики.

---
+++ SearchForFilesOnDisk/Control/FileSearch.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SearchForFilesOnDisk
+{
+    class FileSearch
+    {
+        string path;
+        string searchingFileName;
+        string searchingFileSymbol;
+
+        string fileName;
+
+        Queue<string> foundingPaths = new Queue<string>();
+        Queue<File> foundingFiles = new Queue<File>();
+        Queue<File> passFilterName = new Queue<File>();
+        Queue<File> passFilterSymbolInFile = new Queue<File>();
+
+        bool mathodFoundPaths = false;
+        bool mathodFoundFiles = false;
+        bool mathodFilterName = false;
+        bool Done = false;
+
+        bool Pause = false;
+
+        bool Cancel = false;
+
+        public string Path { get => path; set => path = value; }
+        public string SearchingFileName { get => searchingFileName; set => searchingFileName = value; }
+        public string SearchingFileSymbol { get => searchingFileSymbol; set => searchingFileSymbol = value; }
+        public string FileName { get => fileName; set => fileName = value; }
+
+        public void FoundPaths()
+        {
+            mathodFoundPaths = true;
+            foundingPaths.Clear();
+            foundingPaths.Enqueue(Path);
+            var folders = Directory.EnumerateDirectories(Path);
+            var queue = new Queue<string>(folders);
+            var path = "";
+
+            while(queue.Count != 0)
+            {
+                if (Pause)                
+                    Thread.Sleep(300);                
+                else
+                {
+                    if (Cancel)
+                        break;
+                    {
+                        path = queue.Dequeue();
+                        try
+                        {
+                            foundingPaths.Enqueue(path);
+                            folders = Directory.EnumerateDirectories(path);
+                            foreach (var e in folders)
+                                queue.Enqueue(e);
+                        }
+                        catch (Exception e)
+                        {
+
+                        }
+                    }
+                }
+            }
+
+            mathodFoundPaths = false;
+        }
+
+        public void FoundFiles()
+        {
+            mathodFoundFiles = true;
+            foundingFiles.Clear();
+            IEnumerable<string> files = default(IEnumerable<string>);
+            var folder = "";
+            try
+            {
+                while (foundingPaths.Count != 0 || mathodFoundFiles)
+                {
+                    if (foundingPaths.Count != 0)
+                    {
+                        folder = foundingPaths.Dequeue();
+                        if (folder != null)
+                        {
+                            files = Directory.EnumerateFiles(folder);
+                            foreach (var e in files)
+                                foundingFiles.Enqueue(new File(e, folder));
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            { 
+            
+            }
+            finally { mathodFoundFiles = false; }
+        }
+
+        public void FilterName()
+        {
+
+        }
+    }
+
+    class File
+    {
+        string name;
+        string path;
+
+        public File(string name, string path)
+        {
+            Name = name;
+            Path = path;
+        }
+
+        public string Name { get => name; set => name = value; }
+        public string Path { get => path; set => path = value; }
+    }
+}
---
+++ SearchForFilesOnDisk/View/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace SearchForFilesOnDisk
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.splitter1 = new System.Windows.Forms.Splitter();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.splitter2 = new System.Windows.Forms.Splitter();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 4;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // splitter1
+            // 
+            this.splitter1.Location = new System.Drawing.Point(0, 24);
+            this.splitter1.Name = "splitter1";
+            this.splitter1.Size = new System.Drawing.Size(450, 404);
+            this.splitter1.TabIndex = 5;
+            this.splitter1.TabStop = false;
+            // 
+            // treeView1
+            // 
+            this.treeView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.treeView1.Location = new System.Drawing.Point(12, 27);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(438, 398);
+            this.treeView1.TabIndex = 6;
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // splitter2
+            // 
+            this.splitter2.Location = new System.Drawing.Point(450, 24);
+            this.splitter2.Name = "splitter2";
+            this.splitter2.Size = new System.Drawing.Size(350, 404);
+            this.splitter2.TabIndex = 12;
+            this.splitter2.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(457, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(58, 23);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Папка >";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(521, 30);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(267, 20);
+            this.textBox1.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.splitter2);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.splitter1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.Splitter splitter1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Splitter splitter2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
